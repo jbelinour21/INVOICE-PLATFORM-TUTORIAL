edafcc6c1dcd938d607d7442906bb3344072e5bd
unknown <nourjebeli29@gmail.com>
2021-11-19 13:31:38 +0100

    config heroku2

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 // DB connection
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
-mongoose.connect(process.env.MONGO_DB_URI);
+//mongoose.set("useNewUrlParser", true);
+//mongoose.set("useFindAndModify", false);
+//mongoose.set("useCreateIndex", true);
+//mongoose.set("useUnifiedTopology", true);
+mongoose.connect(process.env.MONGO_DB_URI,{useNewUrlParser: true},{useFindAndModify: false},{useCreateIndex: true},{useUnifiedTopology: true});
 mongoose.connection.on("connected", () => {
   console.log("DB connected");
 });
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use(express.static("./client/build"));
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve("client","build", "index.html"));
+  res.sendFile(path.resolve("client", "build", "index.html"));
 });
 
 //server listening
